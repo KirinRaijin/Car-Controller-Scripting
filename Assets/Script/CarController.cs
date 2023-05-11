5a0ed1c86137c566da5f7dd377353c108b51f0f2
KirinRaijin <rishisivakumaar@gmail.com>
2023-05-11 12:17:10 +0530

    Just added a folder

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+    private bool isBraking;
+
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform rearLeftWheelTransform;
+    public Transform rearRightWheelTransform;
+
+    public float maxSteeringAngle = 30f;
+    public float motorForce = 50f;
+    public float brakeForce = 0f;
+    public float Trx = -0.9f;
+    public float Try = -0.9f;
+    public float Trz = -0.9f;
+
+    void Start()
+    { 
+        GetComponent<Rigidbody>().centerOfMass+= new Vector3(Trx, Try, Trz);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBraking = Input.GetKey(KeyCode.Space);
+    }
+    private void HandleSteering()
+    {
+        steerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steerAngle;
+        frontRightWheelCollider.steerAngle = steerAngle;
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * (motorForce+Time.deltaTime);
+        frontRightWheelCollider.motorTorque = verticalInput * (motorForce + Time.deltaTime);
+
+        brakeForce = isBraking ? 3000f : 0f;
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        rearLeftWheelCollider.brakeTorque = brakeForce;
+        rearRightWheelCollider.brakeTorque = brakeForce;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateWheelPos(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheelPos(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateWheelPos(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateWheelPos(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateWheelPos(WheelCollider wheelCollider, Transform trans)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        trans.rotation = rot;
+        trans.position = pos;
+    }
+
+}
